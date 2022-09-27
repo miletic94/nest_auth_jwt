ef8bfdfccfc72ecfe2760bcd0e9301aa462f31cd
miletic94 <mileticnemanja.lab@gmail.com>
2022-09-27 12:52:22 +0200

    Set up environment.

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeOrmConfig from "../typeorm.config"
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {}
